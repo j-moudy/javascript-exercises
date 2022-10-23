d7e4478df4006c38e1ecc77e417278d2d930a6ad
Jeremy Moudy <jmoudy91@gmail.com>
2022-10-23 16:31:19 -0700

    Exercises in progress

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(s, n) {
+    if (n < 0){
+        return "ERROR";
+    }
+    if (n == 0){
+        return "";
+    }
+    let string = s;
 
+    // start counting from 1, since we copied the value of the 
+    // string the first time already
+    for (let i=1; i<n; i++){
+        string += s;
+    }
+    return string;
 };
 
 // Do not edit below this line
